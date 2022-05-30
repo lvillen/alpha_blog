f0823e176a7db54b674b977ad405fda9960930f0
Luis Villén <l.villenjaldon@gmail.com>
2022-05-30 10:21:05 +0200

    Adds validation and flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,12 +8,16 @@ class ArticlesController < ApplicationController
   end
 
   def new
-  
+    @article = Article.new
   end
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
-    redirect_to @article # == article_path(@article)
+    if @article.save
+      flash[:notice] = "Article was created succesfully."
+      redirect_to @article # == article_path(@article)
+    else
+      render 'new'
+    end
   end
 end
