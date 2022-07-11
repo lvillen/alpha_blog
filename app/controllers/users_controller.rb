61d366e963648b6c50dd0bb8388d04c6dee6c406
lvillen <l.villenjaldon@gmail.com>
2022-07-11 17:56:25 +0200

    Login user when signing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to the Alpha Blog community #{@user.username}!"
       redirect_to articles_path
     else
