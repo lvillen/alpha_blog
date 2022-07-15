63f43ab19290cf3c9b2eda4311064dd1155dd0a7
lvillen <l.villenjaldon@gmail.com>
2022-07-15 12:52:08 +0200

    Adds categories controller and tests

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,27 @@
+class CategoriesController < ApplicationController 
+  def show
+  end
+
+  def index
+  end
+  
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category succesfully created"
+      redirect_to @category
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
